87ae6ff439e9d233440e85107e9940a6b545bb69
GaetanoCrisafulli <gaetanocrisafulli07@gmail.com>
2021-04-06 22:37:44 +0200

    Add: routeParams on channel in constructor

---
+++ src/app/components/channel/channel.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { ChannelComponent } from './channel.component';
+import { ActivatedRouteSnapshot } from '@angular/router';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule
+  ],
+  declarations: [ChannelComponent],
+  exports: [ChannelComponent]
+})
+export class ChannelModule { }
